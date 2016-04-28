48790a2cc5558aea7e1b3fb72b9418c612a2fb0f
Obtuse <theobtuseautodidact@gmail.com>
2016-04-27 20:23:57 -0600

    minor adjustments to  mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "yourmom@herhouse.com"
   layout 'mailer'
 end
--- app/mailers/my_location.rb
+++ app/mailers/my_location.rb
@@ -1,6 +1,6 @@
 class MyLocation < ApplicationMailer
   def send_location(contact, location)
     @location = location
-    mail(to: contact, subject: "my location goes here")
+    mail(to: contact, subject: "My Location")
   end
 end
